181a59bf2bb235a86459e1e195d4550a8b94ae9f
Tanguy Le Floch <lefloch.ta@gmail.com>
2017-08-16 12:16:15 -0500

    Better handling of chapters

--- python/mangas.py
+++ python/mangas.py
@@ -1,16 +1,28 @@
 class Manga:
-    def __init__(self, title, chapters_types, links, chapter_numbers):
-        self.title = title
-        self.chapters_types = chapters_types
-        self.chapter_numbers = chapter_numbers
-        self.links = links
+    def __init__(self, title=None, chapters_types=None, chapters_links=None, chapters_numbers=None, dict=None):
+        if not (dict or (title and chapters_types and chapters_links and chapters_numbers)):
+            raise TypeError("Manga must be constructed either by a dict or the parameters")
+
+        if dict:
+            self.title = dict['title']
+            self.chapters_types = dict['chapters_types']
+            self.chapters_numbers = dict['chapters_numbers']
+            self.chapters_links = dict['chapters_links']
+        else:
+            self.title = title
+            self.chapters_types = chapters_types
+            self.chapters_numbers = chapters_numbers
+            self.chapters_links = chapters_links
+
+    def serialize_chapter(self, index):
+        r_str = '\t' + str(self.chapters_numbers[index]) + ": " + self.chapters_types[index] + " at "
+        r_str += self.chapters_links[index] + '\n'
+
+        return r_str
 
     def serialize(self):
-        chapters_types = ""
+        r_str = self.title + ':\n'
+        for i in range(len(self.chapters_numbers)):
+            r_str += self.serialize_chapter(i)
 
-        for chapt_type in self.chapters_types:
-            chapters_types += chapt_type + ","
-
-        chapters_types = chapters_types[-1]
-
-        return self.title + ", de type(s) : " + chapters_types
+        return r_str
